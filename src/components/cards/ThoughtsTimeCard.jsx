5407079406b679afd1e5225a0095aee443471056
Saurabh Shete <ss.saurabhshete@gmail.com>
2024-10-19 11:25:22 +0530

    fix: changed buld icon

--- src/components/cards/ThoughtsTimeCard.jsx
+++ src/components/cards/ThoughtsTimeCard.jsx
@@ -6,7 +6,7 @@ const ThoughtsTimeCard = () => {
   return (
     <div className="bg-[#F5F5F5] p-6 rounded-lg flex flex-col items-center text-center relative mt-12">
       {/* Half Circle with Glow Effect */}
-      <div className="absolute flex items-center justify-center mb-10 top-[-50px] bg-[#F5F5F5] rounded-full">
+      <div className="absolute flex items-center justify-center mb-6 top-[-40px] bg-[#F5F5F5] rounded-full">
         {/* Lightbulb Icon with glow effect */}
         <div 
         style={{
@@ -21,7 +21,7 @@ const ThoughtsTimeCard = () => {
       </div>
 
       {/* Title */}
-      <h5 className="text-gray-700 font-semibold">Thoughts Time</h5>
+      <h5 className="text-gray-700 font-semibold z-50">Thoughts Time</h5>
 
       {/* Description */}
       <p className="text-gray-500 text-sm mt-2">
